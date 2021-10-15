8d04e6aa620f87ea9be932f3f1b792a460ab17b8
Serkan Mehmet MALAGİÇ <32618577+serkanmalagic@users.noreply.github.com>
2021-10-15 21:37:20 +0300

    initial commit

---
+++ Jenkins/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jenkins
+//
+//  Created by Serkan Mehmet Malagiç on 15.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JenkinsUITests/JenkinsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  JenkinsUITestsLaunchTests.swift
+//  JenkinsUITests
+//
+//  Created by Serkan Mehmet Malagiç on 15.10.2021.
+//
+
+import XCTest
+
+class JenkinsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
