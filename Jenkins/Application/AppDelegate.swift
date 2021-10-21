75d4a01b1d44ccd3a21678b46eadd1a433c24779
Serkan Mehmet MALAGİÇ <32618577+serkanmalagic@users.noreply.github.com>
2021-10-22 00:34:53 +0300

    app apperance added

--- Jenkins/Application/AppDelegate.swift
+++ Jenkins/Application/AppDelegate.swift
@@ -14,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        
+        AppAppearance.setupAppearance()
+
         return true
     }
 
